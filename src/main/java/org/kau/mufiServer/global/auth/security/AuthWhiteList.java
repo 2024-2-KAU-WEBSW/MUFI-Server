64665204f513dd4b59de74a64c0f43c0ab72ac3d
Wang HoEun <hoeunwang@gmail.com>
2024-11-17 00:18:07 +0900

    fix: 카카오 로그인 안되는 부분 주석처리 whitelist 에서

--- src/main/java/org/kau/mufiServer/global/auth/security/AuthWhiteList.java
+++ src/main/java/org/kau/mufiServer/global/auth/security/AuthWhiteList.java
@@ -12,7 +12,7 @@ public class AuthWhiteList {
     );
 
     public static final List<String> AUTH_WHITELIST_WILDCARD = Arrays.asList(
-            "/api/kakao/**", "/api/test/**", "/api/**", //일단 API 다 필터에 안 걸리도록 설정, 나중에 바꾸기
+            "/api/kakao/**", "/api/test/**", //"/api/**", //일단 API 다 필터에 안 걸리도록 설정, 나중에 바꾸기
             "/swagger-ui/**", "/swagger-resources/**", "/api-docs/**",
             "/v3/api-docs/**", "/webjars/**"
     );
