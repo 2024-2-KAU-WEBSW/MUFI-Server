d0741d61dd593698a1b384e0f6306fc8a6e9e9b4
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2024-11-26 14:48:16 +0900

    Develop (#2)
    * chore: 카카오 로그인 및 초기세팅
    
    * chore: Update CD-dev.yml
    
    * chore: Update CD-dev.yml
    
    * chore: CD test
    
    * chore: whitelist 추가
    
    * fix: 카카오 로그인 안되는 부분 주석처리 whitelist 에서
    
    * feat: api 구현 및 api 허용.. 뭐냐 다 함
    
    * feat: CORS 클라 url 추가
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- src/main/java/org/kau/mufiServer/global/config/WebConfig.java
+++ src/main/java/org/kau/mufiServer/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:5173","https://www.mufi-server.o-r.kr","https://mufi.vercel.app/")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:5173","http://localhost:5174","https://www.mufi-server.o-r.kr","https://mufi-client.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
